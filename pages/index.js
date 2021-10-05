c056f57d01fb445eacb2bab3c15d22449b997370
Saddam Satria <ardhisaddam@gmail.com>
2021-10-05 16:20:37 +0700

    Update : ui layout home

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,32 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import { motion } from 'framer-motion';
-import Header from '../components/layout/home/Header';
-// import ServiceContent from '../components/layout/home/ServiceContent';
+// import Header from '../components/layout/home/Header';
+import ServiceContent from '../components/layout/home/ServiceContent';
 // import ChooseUs from '../components/layout/home/ChooseUs';
 // import Teams from '../components/layout/home/Teams';
 // import Services from '../components/layout/home/Services';
 
 const Home = () => {
-  // const [services] = useState([
-  //   {
-  //     name: 'web development',
-  //     subname: 'we develop your website',
-  //     img: '/img/home/icons/web-icons.png',
-  //     alt: 'website development',
-  //   },
-  //   {
-  //     name: 'android developer',
-  //     subname: 'we develop your apps',
-  //     img: '/img/home/icons/android-icons.png',
-  //     alt: 'android development',
-  //   },
-  //   {
-  //     name: 'graphics design',
-  //     subname: 'we design for your brands',
-  //     img: '/img/home/icons/poster-icons.png',
-  //     alt: 'graphics design',
-  //   },
-  // ]);
+  const [services] = useState([
+    {
+      name: 'web development',
+      subname: 'we develop your website',
+      img: '/img/home/icons/web-icons.png',
+      alt: 'website development',
+    },
+    {
+      name: 'android developer',
+      subname: 'we develop your apps',
+      img: '/img/home/icons/android-icons.png',
+      alt: 'android development',
+    },
+    {
+      name: 'graphics design',
+      subname: 'we design for your brands',
+      img: '/img/home/icons/poster-icons.png',
+      alt: 'graphics design',
+    },
+  ]);
 
   // const [teams] = useState([
   //   {
@@ -75,9 +75,9 @@ const Home = () => {
 
   return (
     <motion.div variants={variants} transition={{ duration: 2 }} initial={'out'} animate={'in'}>
-      <Header />
-      {/* <ServiceContent services={services} />
-      <ChooseUs />
+      {/* <Header />  */}
+      <ServiceContent services={services} />
+      {/* <ChooseUs />
       <Teams teams={teams} mouseEnterHandler={mouseEnterHandler} mouseLeaveHandler={mouseLeaveHandler} />
       <Services /> */}
     </motion.div>
