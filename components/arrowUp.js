ce0fe2fd511a2f0270f34990af443a32e9a6dfa3
saddam-satria <ardhisaddam@gmail.com>
2021-10-17 10:17:20 +0700

    Update: Cursor pointer

--- components/arrowUp.js
+++ components/arrowUp.js
@@ -10,8 +10,8 @@ const arrowUp = ({ mode }) => {
 
   return (
     <>
-      <div className="fixed bottom-5 right-8" onClick={onClickHandler}>
-        {!mode ? <Image width={50} height={50} src="/img/home/icons/arrowUp.png" /> : <Image width={50} height={50} src="/img/home/icons/arrowUpDark.png" />}
+      <div className="fixed bottom-5 right-8 cursor-pointer" onClick={onClickHandler}>
+        {mode ? <Image width={50} height={50} src="/img/home/icons/arrowUp.png" /> : <Image width={50} height={50} src="/img/home/icons/arrowUpDark.png" />}
       </div>
       ;
     </>
